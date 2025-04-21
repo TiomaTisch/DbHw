38448b93678ba7c6b55e11e5b8eb74414f6b17df
Tioma <arttisch02@gmail.com>
2025-04-21 15:33:54 +0300

    Initial commit

---
+++ src/main/java/com/example/DbHw1/DbHw1Application.java
@@ -0,0 +1,13 @@
+package com.example.DbHw1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbHw1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbHw1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DbHw1/DbHw1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DbHw1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbHw1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
